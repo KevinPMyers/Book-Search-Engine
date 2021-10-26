333cf29ab1407f72f8b2a3099d3368da8e3db4e0
KevinPMyers <kmyers25@gmail.com>
2021-10-26 18:43:30 -0400

    need to finish typeDefs and resolvers, still working on mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,29 @@
-const resolvers = {
-    Query: {
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
-    }
+const resolvers = {
+
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('-__V -password')
+            .populate('books')
+
+            return userData;
+            }
+        
+
+        throw new AuthenticationError('Not logged in');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne()
+            .select('-__v -password')
+            .populate('books')
+        }
+    },
+    Mutation: {}
 };
 
 module.exports= resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,33 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Query {
-    
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
 }
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type
 `;
 
 module.exports = typeDefs;
