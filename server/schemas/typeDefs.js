0de39de8c6b55977ad81fbb23d4cf2c75fe0afa3
KevinPMyers <kmyers25@gmail.com>
2021-10-27 17:57:45 -0400

    typeDefs finalized

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,7 +27,23 @@ type Query {
     me: User
 }
 
-type
+input bookInput {
+    bookId: String
+    authors: [String]
+    title: String
+    image: String
+    description: String
+    link: String
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: bookInput!): User
+    removeBook(bookId: ID!): User
+}
+
+
 `;
 
 module.exports = typeDefs;
